af447178cacfc253165bded39f92ab5974a548fd
dfandakov <danielfandakov@billericak12.com>
2023-09-14 09:45:07 -0400

    Commit

---
+++ Yummy.java
@@ -0,0 +1,25 @@
+public class Yummy {
+    public static void main(String [] args){
+        System.out.println("Yummy");
+        chocolate();
+        frenchFries();
+        System.out.println("done");
+    }
+    public static void chocolate(){
+        System.out.print("Chocolate ");
+        System.out.println("Cake");
+
+    }
+    public static void frenchFries(){
+        System.out.println("french fries with ketchup");
+        chocolate();
+        System.out.println("that was yummy");
+    }
+    public static void iceCream(){
+        System.out.println("ice");
+        frenchFries();
+        System.out.println("cream");
+    }
+    
+}
+
