0bc8c3380c9c2aa4f6a0c4d34178495f7a218d5d
dfandakov <danielfandakov@billericak12.com>
2023-09-22 09:29:55 -0400

    Commit

--- MathEx.java
+++ MathEx.java
@@ -1,15 +1,27 @@
 public class MathEx {
-    pubic statid void main(String [] args){
+    public static void main(String [] args){
+       System.out.println(swap(123));
+       System.out.println(swap(424));
+       System.out.println(swap(726));
 
+   
+   
+   
+   
     }
  
  
  
  
-    public static void swap (int y){
-        int ones = y%
+    public static int swap (int y){
+        int onesValue = y%10;
+        int tensValue = y%100/10;
+        int hundredsValue = y/100;
+        int newNumber = hundredsValue*100 + onesValue * 10 + tensValue;
+        return newNumber;
+        
+    }
 
- }
 
 
 
