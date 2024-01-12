637fb252f4b3f7c6843dbf20bd1959b93bc1bf26
dfandakov <danielfandakov@billericak12.com>
2024-01-12 10:23:11 -0500

    Commit

--- Strings.java
+++ Strings.java
@@ -8,6 +8,10 @@ public static String scroll(String a){
     return c + b;
 }
 
+
+
+
+
 public static String convertName(String fullname){
 int c = fullname.indexOf(",");
 String f = fullname.substring(c+1);
@@ -19,7 +23,40 @@ return f1+" "+l;
 
 }
 
-public static String 
+
+
+
+public static String negativeString(String negative){//0010111001
+    
+    String n = negative.replace("0" , "2");
+    String n1 = negative.replace("2" , "0");
+
+    r
+
+
+
+}
+
+public static String convertDate(String date){
+    int d = date.indexOf("/");
+    int c = date.lastIndexOf("/");
+    String b = date.substring(0,d);
+    String l = date.substring(d,c);
+    String f = date.substring(c);
+
+   String l1 = l.trim();
+   String b1 = b.trim();
+   String f1 = f.trim();
+
+   String b2 = b1.replace("/" , "-");
+ 
+
+
+   String finalDate = l1 + "-" + b2 + "-" + f1;
+
+   return finalDate.replace("/", "");
+
+   }
 
 public static void main(String[] args){
     System.out.println(scroll("Hello World"));
@@ -28,10 +65,16 @@ public static void main(String[] args){
     
 
 
+    System.out.println(negativeString("0010111001"));
+    System.out.println(negativeString("11111111"));
+
+
     System.out.println(convertName(" Reubenstein, Lori Renee "));
     System.out.println(convertName("Biden,Joe"));
     System.out.println(convertName("the Clown, Bozo"));
 
+
+    System.out.println("04/20/2014 becomes " + convertDate("04/20/2014"));
 
 
 
